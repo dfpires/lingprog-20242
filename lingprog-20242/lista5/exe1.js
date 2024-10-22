d77400b86cbdad7dc64235ede375d9d6aaa49232
Daniel Pires <dfpires@gmail.com>
2024-10-22 07:56:02 -0300

    Add files via upload

--- lingprog-20242/lista5/exe1.js
+++ lingprog-20242/lista5/exe1.js
@@ -1,28 +1,55 @@
-let alunos = [
-    {nome: "Paulo", idade:17, nota: 8.5},
-    {nome: "Pedro", idade:18, nota: 8.0},
-    {nome: "Julio", idade:19, nota: 9.5},
-    {nome: "Maria", idade:20, nota: 9.0},
-    {nome: "Carlos", idade:21, nota: 7.5},
-]
+const prompt = require('prompt-sync')()
 
-console.log(`Lista de todos os alunos`)
-for(let i=0;i<alunos.length;i++){
-    console.log(`Nome: ${alunos[i].nome} e a nota: ${alunos[i].nota}`)
+function cadastraAluno(vetObj){
+    vetObj.push({
+        nome: prompt(`Informe nome do aluno`),
+        idade: Number(prompt(`Informe a idade do aluno`)),
+        nota: Number(prompt(`Informe a nota do aluno`))
+    })
+    console.log(`Aluno foi inserido com sucesso`)
 }
-
-console.log(`Aluno com a maior nota`)
-let objMaiorNota = alunos[0]
-for(let i=0;i<alunos.length;i++){
-    if (alunos[i].nota > objMaiorNota.nota){
-        objMaiorNota = alunos[i]
+function consultaAlunos(vetObj){
+    console.log(`Lista de todos os alunos`)
+    for(let i=0;i<vetObj.length;i++){
+        console.log(`Nome: ${vetObj[i].nome} e a nota: ${vetObj[i].nota}`)
     }
 }
-console.log(objMaiorNota)
-
-console.log(`A média de notas de todos os alunos é`)
-let soma = 0
-for(let i=0;i<alunos.length;i++){
-    soma += alunos[i].nota
+function maiorNota(vetObj){
+    console.log(`Aluno com a maior nota`)
+    let objMaiorNota = vetObj[0]
+    for(let i=0;i<vetObj.length;i++){
+        if (vetObj[i].nota > objMaiorNota.nota){
+            objMaiorNota = vetObj[i]
+        }
+    }
+    console.log(objMaiorNota)
 }
-console.log(soma/alunos.length)
+
+function mediaNotas(vetObj){
+    console.log(`A média de notas de todos os alunos é`)
+    let soma = 0
+    for(let i=0;i<vetObj.length;i++){
+        soma += vetObj[i].nota
+    }
+    console.log(soma/vetObj.length)
+}
+
+function principal(){
+    // variável alunos tem escopo local - existe somente dentro da função principal
+    let alunos = [
+        {nome: "Paulo", idade:17, nota: 8.5},
+        {nome: "Pedro", idade:18, nota: 8.0},
+        {nome: "Julio", idade:19, nota: 9.5},
+        {nome: "Maria", idade:20, nota: 9.0},
+        {nome: "Carlos", idade:21, nota: 7.5},
+    ]
+    cadastraAluno(alunos)
+    consultaAlunos(alunos)    
+    maiorNota(alunos)
+    mediaNotas(alunos)
+}
+
+principal()
+
+
+
